705e9b9b6e5b79159a26ddb81dbc5d0226b84b53
PokeMasterCP <christian.adrn.perez@gmail.com>
2026-08-29 20:39:22 -0500

    Add application dashboard and database schema

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"embed"
+	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
+//go:embed web/templates/*.html web/static
+var webFS embed.FS
+
 func main() {
+	templates, err := template.ParseFS(webFS, "web/templates/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	staticFS, err := fs.Sub(webFS, "web/static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
 	mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		_, _ = w.Write([]byte("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>Jobby</title></head><body><h1>Hello, World!</h1></body></html>"))
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			log.Printf("render overview: %v", err)
+		}
 	})
 
 	addr := ":8080"
